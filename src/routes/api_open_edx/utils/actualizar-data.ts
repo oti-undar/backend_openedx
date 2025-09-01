1ae3d2610717f0f2acb54286523bdfe9de248585
Arturo Rodas <arturo.rodas.gonzales@gmail.com>
2025-08-31 21:11:41 -0500

    Actualizar data

--- src/routes/api_open_edx/utils/actualizar-data.ts
+++ src/routes/api_open_edx/utils/actualizar-data.ts
@@ -1,7 +1,5 @@
 import type { getUserIdSchemaProps } from '@/schemas/validation.js'
 import { db, db_open_edx } from '@/db/db.js'
-import { ROLES } from '@/lib/constantes.js'
-import type { UsuarioCurso } from '@prisma/client'
 
 type Alumno = {
   user_id: number
@@ -14,21 +12,34 @@ type Alumno = {
   email: string
 }
 
+type Instructor = {
+  instructor_id: number
+  instructor_is_superuser: number
+  instructor_is_staff: number
+  instructor_is_active: number
+  instructor_username: string
+  instructor_first_name: string
+  instructor_last_name: string
+  instructor_email: string
+}
+
 type Grouped = {
   course_id: string
   display_name: string
   alumnos: Alumno[]
+  instructor: Instructor
 }
 
 export async function actualizarData({ item }: { item: getUserIdSchemaProps }) {
   const userId = item.user_id
 
   const data = await db_open_edx.$queryRaw<
-    (Omit<Grouped, 'alumnos'> & Alumno)[]
+    (Omit<Grouped, 'alumnos' | 'instructor'> & Alumno & Instructor)[]
   >`
     SELECT 
     e.course_id,
     c.display_name,
+
     u.id AS user_id,
     u.is_superuser,
     u.is_staff,
@@ -36,7 +47,17 @@ export async function actualizarData({ item }: { item: getUserIdSchemaProps }) {
     u.username,
     u.first_name,
     u.last_name,
-    u.email
+    u.email,
+    
+    iu.id AS instructor_id,
+    iu.is_superuser AS instructor_is_superuser,
+    iu.is_staff AS instructor_is_staff,
+    iu.is_active AS instructor_is_active,
+    iu.username AS instructor_username,
+    iu.first_name AS instructor_first_name,
+    iu.last_name AS instructor_last_name,
+    iu.email AS instructor_email
+    
     FROM student_courseaccessrole ar
     JOIN student_courseenrollment e 
     ON e.course_id = ar.course_id
@@ -56,10 +77,39 @@ export async function actualizarData({ item }: { item: getUserIdSchemaProps }) {
 
   const result = Object.values(
     data.reduce<Record<string, Grouped>>(
-      (acc, { course_id, display_name, ...alumno }) => {
+      (
+        acc,
+        {
+          course_id,
+          display_name,
+          instructor_id,
+          instructor_is_superuser,
+          instructor_is_staff,
+          instructor_is_active,
+          instructor_username,
+          instructor_first_name,
+          instructor_last_name,
+          instructor_email,
+          ...alumno
+        }
+      ) => {
         const key = `${course_id}-${display_name}`
         if (!acc[key]) {
-          acc[key] = { course_id, display_name, alumnos: [] }
+          acc[key] = {
+            course_id,
+            display_name,
+            alumnos: [],
+            instructor: {
+              instructor_id,
+              instructor_is_superuser,
+              instructor_is_staff,
+              instructor_is_active,
+              instructor_username,
+              instructor_first_name,
+              instructor_last_name,
+              instructor_email,
+            },
+          }
         }
         acc[key].alumnos.push(alumno)
         return acc
@@ -70,7 +120,32 @@ export async function actualizarData({ item }: { item: getUserIdSchemaProps }) {
 
   db.$transaction(async prisma => {
     for (const item of result) {
-      const { course_id, display_name, alumnos } = item
+      const { course_id, display_name, alumnos, instructor } = item
+
+      const user = await prisma.user.upsert({
+        where: {
+          id: instructor.instructor_id,
+        },
+        update: {
+          is_superuser: instructor.instructor_is_superuser === 1,
+          is_staff: instructor.instructor_is_staff === 1,
+          is_active: instructor.instructor_is_active === 1,
+          username: instructor.instructor_username,
+          first_name: instructor.instructor_first_name,
+          last_name: instructor.instructor_last_name,
+          email: instructor.instructor_email,
+        },
+        create: {
+          id: instructor.instructor_id,
+          is_superuser: instructor.instructor_is_superuser === 1,
+          is_staff: instructor.instructor_is_staff === 1,
+          is_active: instructor.instructor_is_active === 1,
+          username: instructor.instructor_username,
+          first_name: instructor.instructor_first_name,
+          last_name: instructor.instructor_last_name,
+          email: instructor.instructor_email,
+        },
+      })
 
       await prisma.curso.upsert({
         where: {
@@ -88,7 +163,7 @@ export async function actualizarData({ item }: { item: getUserIdSchemaProps }) {
       await prisma.usuarioCurso.upsert({
         where: {
           user_id_curso_id: {
-            user_id: userId,
+            user_id: user.id,
             curso_id: course_id,
           },
         },
@@ -96,7 +171,7 @@ export async function actualizarData({ item }: { item: getUserIdSchemaProps }) {
           is_instructor: true,
         },
         create: {
-          user_id: userId,
+          user_id: user.id,
           curso_id: course_id,
           is_instructor: true,
         },
