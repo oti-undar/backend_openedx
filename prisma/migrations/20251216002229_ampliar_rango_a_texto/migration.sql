c43b1f115911827d4609c15456b2e5596e212b0d
Arturo Rodas <arturo.rodas.gonzales@gmail.com>
2025-12-15 19:23:15 -0500

    ampliar a text

---
+++ prisma/migrations/20251216002229_ampliar_rango_a_texto/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE `examen` MODIFY `title` TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE `pregunta` MODIFY `title` TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE `respuesta` MODIFY `respuesta` TEXT NOT NULL;
